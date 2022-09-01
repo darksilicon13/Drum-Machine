ca2ba1abb172be5ce216bb16669c0fc0ecfe034c
darksilicon13 <darksilicon@outlook.com>
2022-09-01 23:42:21 +0900

    pad sound operational

--- src/components/Pad.jsx
+++ src/components/Pad.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './Pad.css';
+import {setDisplay} from '../redux/modules/drumSlice'
 
 const Pad = ({alphakey, name, src}) => {
+    const dispatch = useDispatch();
+
+    const {power, volume} = useSelector(state=>state.drum);
+
+    const onPadClick = () => {
+        if(!power) {
+            return;
+        }
+        const audio = new Audio(document.getElementById(alphakey).src);
+        audio.volume = volume/100;
+        audio.play();
+        
+        dispatch(setDisplay(name));
+        setTimeout(()=>{dispatch(setDisplay(''))},2000);
+    }
 
     return (
         // User Story #4: Within each .drum-pad, 
@@ -9,8 +26,8 @@ const Pad = ({alphakey, name, src}) => {
         // a class name of clip, and an id corresponding to the inner text of its parent .drum-pad (e.g. id="Q", id="W", id="E" etc.).
         // User Story #5: When I click on a .drum-pad element, 
         // the audio clip contained in its child audio element should be triggered.
-        <button className="drum-pad">{alphakey}
-            <audio className="clip" src={src}/>
+        <button className="drum-pad" onClick={onPadClick}>{alphakey}
+            <audio id={alphakey} className="clip" src={src}/>
         </button>
     );
 }
